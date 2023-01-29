539770fa55233382a45a094bad8bdadc2ca6418c
tinbob655 tinbob655 <wrutland07@gmail.com>
2023-01-29 21:48:03 +0000

    Site setup

--- scripts.js
+++ scripts.js
@@ -1,7 +1,25 @@
 function init() {
-    //immediatly after file is loaded
+    setTimeout(() => {
+        onDOMLoad();
+    }, 1000);
 };
 
-function onDOMload() {
-    //after the DOM has been loaded
+function onDOMLoad() {
+    const header = document.getElementById('header');
+    loadHeader();
 };
+
+function loadHeader() {
+    header.innerHTML = `
+    <div class="headerLink">
+        <p>
+            oi
+        </p>
+    </div>`
+}
+
+function loadFooter() {
+    //make sure this <div>Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0</div> is in footer
+};
+
+window.onload = init();
