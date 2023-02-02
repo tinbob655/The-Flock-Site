4028c57a8ab2cb9621d6eb1eb2e1c8aa0413de97
tinbob655 <wrutland07@gmail.com>
2023-02-02 19:00:08 +0000

    Upgraded favicon integration

--- scripts.js
+++ scripts.js
@@ -22,6 +22,16 @@ function init() {
 function DOMloaded() {
     const header = document.getElementById('header');
     const footer = document.getElementById('footer');
+    document.head.innerHTML = document.head.innerHTML +`
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon-stuff/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-stuff/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-stuff/favicon-16x16.png">
+    <link rel="manifest" href="/favicon-stuff/site.webmanifest">
+    <link rel="mask-icon" href="/favicon-stuff/safari-pinned-tab.svg" color="#000000">
+    <link rel="shortcut icon" href="/favicon-stuff/favicon.ico">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="msapplication-config" content="/favicon-stuff/browserconfig.xml">
+    <meta name="theme-color" content="#000000">`;
     loadHeader();
     loadFooter();
     document.body.style.opacity = 1.0;
