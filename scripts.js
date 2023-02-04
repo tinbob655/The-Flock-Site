c85ef47dec8c10ea893a88279066979aa5e214f9
tinbob655 <wrutland07@gmail.com>
2023-02-04 16:19:08 +0000

    Massive upgrade to favicon application method

--- scripts.js
+++ scripts.js
@@ -14,6 +14,19 @@ function init() {
 };
 
 function DOMloaded() {
+    //load favicon info
+    document.head.insertAdjacentHTML('beforeend', `
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon-stuff/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-stuff/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-stuff/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicon-stuff/site.webmanifest"/>
+        <link rel="mask-icon" href="/favicon-stuff/safari-pinned-tab.svg" color="#000000"/>
+        <link rel="shortcut icon" href="/favicon-stuff/favicon.ico"/>
+        <meta name="msapplication-TileColor" content="#000000"/>
+        <meta name="msapplication-config" content="/favicon-stuff/browserconfig.xml"/>
+        <meta name="theme-color" content="#000000"/>) `);
+
+
     //if the user is mobile then adjust css variables accordingly
     if (isMobile() == true) {
         const root = document.querySelector(':root');
